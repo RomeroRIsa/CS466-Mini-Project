cc1b17939971d78857d9650013f953d5177a7eb7
RomeroRIsa <rrisa2@illinois.edu>
2022-04-05 17:43:41 -0500

    commit

--- generate.py
+++ generate.py
@@ -1,1 +1,32 @@
-#Generate things
+import numpy as np
+
+def generate_sequences(sequence_count, sequence_length):
+    """
+    @param sequence_count: number of sequences to be generated
+    @param sequence_length: length of each seqeunce
+    """
+    sequences = [] #list of sequences
+    sequence_array = np.random.rand(sequence_count, sequence_length) #np array of random number [0,1)
+    
+    for i in range(sequence_array.shape[0]):
+        sequence_string = ""
+        for j in range(sequence_array.shape[1]):
+            if sequence_array[i][j] < 0.25:
+                sequence_string += "A"
+            elif sequence_array[i][j] < 0.5:
+                sequence_string += "C"
+                
+            elif sequence_array[i][j] < 0.75:
+                sequence_string += "G"
+            else:
+                sequence_string += "T"
+        sequences.append(sequence_string)
+            
+    return sequences
+    
+
+sc = int(input("Enter sequence count: ")) #asks user for sequence count
+sl = int(input("Enter sequence length: ")) #asks user for sequence length
+
+mySequences = generate_sequences(sc, sl) #generate
+print(mySequences) 
