481cf1dba5db537a621f51c34d8cfc1c9e81701d
Parth Shukla <pshukl21@illinois.edu>
2022-04-09 14:55:10 -0500

    generate_motif

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+from random import random
 import numpy as np
 
 def generate_sequences(sequence_count, sequence_length):
@@ -25,41 +26,86 @@ def generate_sequences(sequence_count, sequence_length):
     return sequences
     
 
-sc = int(input("Enter sequence count: ")) #asks user for sequence count
-sl = int(input("Enter sequence length: ")) #asks user for sequence length
+#sc = int(input("Enter sequence count: ")) #asks user for sequence count
+#sl = int(input("Enter sequence length: ")) #asks user for sequence length
 
-mySequences = generate_sequences(sc, sl) #generate
-print(mySequences) 
+#mySequences = generate_sequences(sc, sl) #generate
+#print(mySequences) 
 
 
 def generate_motif(ICPC, ML, SC):
-
+    
     motif = []
 
     icpcDict = {1 : .8105, 1.5 : .9245, 2 : 1}
-    randNum = np.random(0,1)
-    p = icpcDict[ICPC]
-
-    probA = (1-p)/3
-    probC = (1-p)/3
-    probG = (1-p)/3
-    probT = (1-p)/3
-
-    if (randNum < .25):
-        probA = icpcDict[ICPC]
-    elif (randNum < .55):
-        probC = icpcDict[ICPC]
-    elif (randNum < .75):
-        probG = icpcDict[ICPC]
-    else:
-        probT = icpcDict[ICPC]
+    randNum = random()
 
     for i in range(0, ML):
-        seq = [0,0,0,0]
+        p = icpcDict[ICPC]
+        otherP = (1-p)/3
+
+        probA = (1-p)/3
+        probC = (1-p)/3
+        probG = (1-p)/3
+        probT = (1-p)/3
+
+        preffered = ""
+        second = ""
+        third = ""
+        fourth = ""
+
+        if (randNum < .25):
+            probA = icpcDict[ICPC]
+            preffered = "A"
+            second = "C"
+            third = "G"
+            fourth = "T"
+
+        elif (randNum < .50):
+            probC = icpcDict[ICPC]
+            preffered = "C"
+            second = "A"
+            third = "G"
+            fourth = "T"
+        elif (randNum < .75):
+            probG = icpcDict[ICPC]
+            preffered = "G"
+            second = "A"
+            third = "C"
+            fourth = "T"
+        else:
+            probT = icpcDict[ICPC]
+            preffered = "T"
+            second = "A"
+            third = "G"
+            fourth = "C"
+
+        
+        seq = {"A" : 0, "G" : 0, "C" : 0, "T" : 0}
         for j in range(0, SC):
-            randNum = np.random(0,1)
-            if(randNum <= p):
+            randNum = random()
+            if(randNum < p):
+                seq[preffered] += 1
+            elif(randNum < p + otherP):
+                seq[second] += 1
+            elif(randNum < p + 2*otherP):
+                seq[third] += 1
+            else:
+                seq[fourth] += 1
+
+
+        seqAdd = [seq["A"], seq["G"], seq["C"], seq["T"]]
+
+        motif.append(seqAdd)
+
                 
+    return motif
+
+print(generate_motif(1, 6, 20))
+
+
+
+
             
 
     
