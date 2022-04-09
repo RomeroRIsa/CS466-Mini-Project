c42c28a64c5290cad92055a7c8d43d276e8fcdae
RomeroRIsa <rrisa2@illinois.edu>
2022-04-09 16:34:48 -0500

    commit

--- generate.py
+++ generate.py
@@ -7,9 +7,10 @@ def generate_sequences(sequence_count, sequence_length):
     """
     @param sequence_count: number of sequences to be generated
     @param sequence_length: length of each seqeunce
+    @return: sequences which is a list of strings of A, C, G, and Ts
     """
     sequences = [] #list of sequences
-    sequence_array = np.random.rand(sequence_count, sequence_length) #np array of random number [0,1)
+    sequence_array = np.random.rand(sequence_count, sequence_length) #np array of random number [0,1) of shape (sc, sl)
     
     for i in range(sequence_array.shape[0]):
         sequence_string = ""
@@ -32,143 +33,28 @@ def generate_binding_sites(sequence_count, sequence_length, motif_length):
     @param sequence_count: number of sequences to be generated
     @param sequence_length: length of each seqeunce
     @param motif_length: length of each motif
+    @return: np array of integers of size sequence count
     """
     
     return np.random.randint(0, sequence_length-motif_length-1, size=sequence_count)
-    
-
-sl = 500 #default value for sequence length
-sc = [5, 10, 20] #possible sequence counts
-ml = [6, 7, 8] #possible motif lengths
-sc_default = ['default', 'ICPC_1', 'ICPC_1.5', 'ML_6', 'ML_7'] #directories that will have SC = 10 (default)
-dirname = os.path.dirname(__file__) #path of generate.py
-
-#generate 10 sequences and binding sites for directories in sc_default
-for directory in sc_default:
-    for i in range(1, 11):
-        #join for relative path to file
-        sequence_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/sequences.fasta') 
-        site_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/sites.txt')
-        motif_length_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/motiflength.txt')
-        
-        mySequences = generate_sequences(sc[1], sl) #generate sequence
-        
-        if directory == 'ML_6':
-            mySites = generate_binding_sites(sc[1], sl, ml[0])
-            myMotifLength = ml[0]
-        elif directory == 'ML_7':
-            mySites = generate_binding_sites(sc[1], sl, ml[1])
-            myMotifLength= ml[1]
-        else:
-            mySites = generate_binding_sites(sc[1], sl, ml[2])
-            myMotifLength = ml[2]
-        
-        #open files    
-        fasta_file = open(sequence_file, "w")
-        sites = open(site_file, "w") 
-        motif_length = open(motif_length_file, "w")
-        
-        for j in range(len(mySequences)):
-            fasta_file.write(">" + "Sequence " + str(j+1) + "\n" + mySequences[j] + "\n") #write to fasta file
-            sites.write(str(mySites[j]) + "\n") #write to sites.txt
-        motif_length.write(str(myMotifLength))
-          
-        #close files  
-        fasta_file.close()
-        sites.close()
-
-#generate 10 sequences and binding sites for SC = 5
-for i in range(1, 11):
-    #join for relative path to file
-    sequence_file = os.path.join(dirname, 'dataset/SC_5/' + str(i) + '/sequences.fasta')
-    site_file = os.path.join(dirname, 'dataset/SC_5/' + str(i) + '/sites.txt')
-    motif_length_file = motif_length_file = os.path.join(dirname, 'dataset/SC_5/' + str(i) + '/motiflength.txt')
-    
-    mySequences = generate_sequences(sc[0], sl) #generate sequence
-    mySites = generate_binding_sites(sc[0], sl, ml[2])
-    myMotifLength = ml[2]
-    
-    
-    #open files
-    fasta_file = open(sequence_file, "w")
-    sites = open(site_file, "w") 
-    motif_length = open(motif_length_file, "w") 
-    
-    for j in range(len(mySequences)):
-        fasta_file.write(">" + "Sequence " + str(j+1) + "\n" + mySequences[j] + "\n") #write to fasta file
-        sites.write(str(mySites[j]) + "\n") #write to sites.txt
-    motif_length.write(str(myMotifLength))
-    
-    #close files
-    fasta_file.close()
-    sites.close()
-    motif_length.close()
-
-#generate 10 sequences and binding sites for SC = 20
-for i in range(1, 11):
-    #join for relative path to file
-    sequence_file = os.path.join(dirname, 'dataset/SC_20/' + str(i) + '/sequences.fasta')
-    site_file = os.path.join(dirname, 'dataset/SC_20/' + str(i) + '/sites.txt')
-    motif_length_file = os.path.join(dirname, 'dataset/SC_20/' + str(i) + '/motiflength.txt')
-    
-    mySequences = generate_sequences(sc[2], sl) #generate sequence
-    mySites = generate_binding_sites(sc[2], sl, ml[2])
-    myMotifLength = ml[2]
-    
-    #open files
-    fasta_file = open(sequence_file, "w") 
-    sites = open(site_file, "w")
-    motif_length = open(motif_length_file, "w") 
-    
-    for j in range(len(mySequences)):
-        fasta_file.write(">" + "Sequence " + str(j+1) + "\n" + mySequences[j] + "\n") #write to fasta file
-        sites.write(str(mySites[j]) + "\n")
-    motif_length.write(str(myMotifLength))
-      
-    #close files  
-    fasta_file.close()
-    sites.close()
-    motif_length.close()
-
-
-def plant_sites(sequences, sites, motif):
-    ML = np.sum(motif[0])
-    motif = motif/ML
-    nuc_dict = {0: 'A', 1:'C', 2:'G', 3:'T'}
-    planted_sequences = []
-    for i, (sequence, site) in enumerate(zip(sequences,sites)):
-        seqlist = list(sequence)
-        for i, nuc in enumerate(motif):
-            seqlist[i+site] = nuc_dict[np.random.choice(np.arange(4), 1, p=nuc)[0]]
-        planted_sequences.append(''.join(seqlist))
-    return planted_sequences
-
-
-#sc = int(input("Enter sequence count: ")) #asks user for sequence count
-#sl = int(input("Enter sequence length: ")) #asks user for sequence length
-
-#mySequences = generate_sequences(sc, sl) #generate
-#print(mySequences) 
-
-ML = 5
-SC = 20
-ICPC = 1
 
 def generate_motif(ICPC, ML, SC):
-    
+    """
+    @param ICPC: information count per column
+    @ML: motif length
+    @SC: sequence count
+    Used the algorithm given in the appendix of the mini project PDF
+    @return: a list of lists with integers as the element
+    """
     motif = []
 
-    icpcDict = {1 : .8105, 1.5 : .9245, 2 : 1}
+    icpcDict = {1 : .8105, 1.5 : .9245, 2 : 1} #probabilities of each ICPC
     randNum = random()
 
+    #select preferred nucleotide
     for i in range(0, ML):
         p = icpcDict[ICPC]
         otherP = (1-p)/3
-
-        probA = (1-p)/3
-        probC = (1-p)/3
-        probG = (1-p)/3
-        probT = (1-p)/3
 
         preffered = ""
         second = ""
@@ -176,32 +62,28 @@ def generate_motif(ICPC, ML, SC):
         fourth = ""
 
         if (randNum < .25):
-            probA = icpcDict[ICPC]
             preffered = "A"
             second = "C"
             third = "G"
             fourth = "T"
 
         elif (randNum < .50):
-            probC = icpcDict[ICPC]
             preffered = "C"
             second = "A"
             third = "G"
             fourth = "T"
         elif (randNum < .75):
-            probG = icpcDict[ICPC]
             preffered = "G"
             second = "A"
             third = "C"
             fourth = "T"
         else:
-            probT = icpcDict[ICPC]
             preffered = "T"
             second = "A"
             third = "G"
             fourth = "C"
 
-
+        #add 1 to nucleotide count based on probability
         seq = {"A" : 0, "G" : 0, "C" : 0, "T" : 0}
         for j in range(0, SC):
             randNum = random()
@@ -214,28 +96,93 @@ def generate_motif(ICPC, ML, SC):
             else:
                 seq[fourth] += 1
 
-
         seqAdd = [seq["A"], seq["G"], seq["C"], seq["T"]]
 
         motif.append(seqAdd)
 
     return motif
 
-motif = (generate_motif(ICPC, ML, SC))
+def plant_sites(sequences, sites, motif):
+    """
+    @param sequences: a list of strings, each string is a sequence to be planted
+    @sites: an np array of integers
+    @motif: a list of lists with integers as elements (PWM)
+    @return: a list of sequences with the motifs planted
+    """
+    ML = np.sum(motif[0]) 
+    motif = motif/ML
+    nuc_dict = {0: 'A', 1:'C', 2:'G', 3:'T'}
+    planted_sequences = []
+    for i, (sequence, site) in enumerate(zip(sequences,sites)):
+        seqlist = list(sequence)
+        for i, nuc in enumerate(motif):
+            seqlist[i+site] = nuc_dict[np.random.choice(np.arange(4), 1, p=nuc)[0]]
+        planted_sequences.append(''.join(seqlist))
+    return planted_sequences
 
-def convert_motif(ML, motif):
-    with open('motif.txt', 'w') as f:
-        f.write('<MOTIF1\t' + str(ML))
+def convert_motif(ML, motif, file):
+    """
+    @param ML: integer, motif length
+    @param motif: a list of lists with integers as elements
+    @param file: a string containing relative path to the file
+    """
+    with open(file, 'w') as f:
+        f.write('>MOTIF1\t' + str(ML))
         for i in range(len(motif)):
             f.write('\n')
             for j in range(len(motif[i])):
                 f.write(str(motif[i][j]) + '\t')
         f.write('\n<')
 
-convert_motif(ML, motif)
+sl = 500 #default value for sequence length
+sc_default = ['default', 'ICPC_1', 'ICPC_1.5', 'ML_6', 'ML_7', 'SC_5', 'SC_20'] #directories that will have SC = 10 (default)
+#dict that maps each directory with tuple of parameters (ICPC, ML, SC)
+directory_dict = {'default': (2, 8, 10),
+                 'ICPC_1': (1, 8, 10),
+                 'ICPC_1.5': (1.5, 8, 10),
+                 'ML_6': (2, 6, 10),
+                 'ML_7': (2, 7, 10),
+                 'SC_5': (2, 8, 5),
+                 'SC_20': (2, 8, 20) 
+                 }
+dirname = os.path.dirname(__file__) #path of generate.py
+
+#generate 10 sequences and binding sites for directories in sc_default
+for directory in sc_default:
+    for i in range(1, 11):
+        #join for relative path to file
+        sequence_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/sequences.fasta') 
+        site_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/sites.txt')
+        motif_length_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/motiflength.txt')
+        motif_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/motif.txt')
+        
+        #generate
+        mySequences = generate_sequences(directory_dict[directory][2], sl)
+        mySites = generate_binding_sites(directory_dict[directory][2], sl, directory_dict[directory][1])
+        myMotifLength = directory_dict[directory][1]
+        myMotif = generate_motif(directory_dict[directory][0], directory_dict[directory][1], directory_dict[directory][2])
+        plantedSequences = plant_sites(mySequences, mySites, myMotif)
+        
+        #open files    
+        fasta_file = open(sequence_file, "w")
+        sites = open(site_file, "w") 
+        motif_length = open(motif_length_file, "w")
+        motif = open(motif_file, "w")
+        
+        #write into files
+        for j in range(len(plantedSequences)):
+            fasta_file.write(">" + "Sequence " + str(j+1) + "\n" + plantedSequences[j] + "\n") #write to fasta file
+            sites.write(str(mySites[j]) + "\n") #write to sites.txt 
+        motif_length.write(str(myMotifLength))
+        convert_motif(directory_dict[directory][1], myMotif, motif_file)
+        
+        #close files  
+        fasta_file.close()
+        sites.close()
+        motif_length.close()
 
 
-print(generate_motif(1, 6, 20))
+
 
 
 
