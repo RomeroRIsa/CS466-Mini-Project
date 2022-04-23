b4d1b28f2fe61304f55396ade2c7c0e0f49dae94
mcheng15 <mcheng15@illinois.edu>
2022-04-23 15:25:48 -0500

    basic file loop

--- motif_finder.py
+++ motif_finder.py
@@ -1,7 +1,9 @@
 from random import random
 import numpy as np
 import os
+from Bio import SeqIO
 import math
+import timeit
 
 def find_best_pos(sequence, motif, background, ML):
     best_pos = 0
@@ -16,41 +18,75 @@ def find_best_pos(sequence, motif, background, ML):
                 break
             score += math.log(motif[j-i, nuc]/background[nuc])
         if score > best_score:
-            score = best_score
+            best_score = score
             best_pos = i
     return best_pos
 
-
 def gibbs_find_motif(sequence_strs, ML):
-	sequence_list = np.array([[i for i in x] for x in sequence_strs])
-	background = np.zeros((sequence_list.shape[0], 4))
-	background[:,0] = np.count_nonzero(sequence_list=="A", axis=1)
-	background[:,1] = np.count_nonzero(sequence_list=="C", axis=1)
-	background[:,2] = np.count_nonzero(sequence_list=="G", axis=1)
-	background[:,3] = np.count_nonzero(sequence_list=="T", axis=1)
-	background = background/sequence_list.shape[1]
-	sequence_pos = np.random.randint(0, sequence_list.shape[1]-ML+1, (sequence_list.shape[0]))
-	prev_sequence_pos = np.full_like(sequence_pos, -1)
-	motif = np.zeros((ML, 4))
-	while not np.array_equal(prev_sequence_pos, sequence_pos):
-	    prev_sequence_pos = sequence_pos
-	    for i in range(sequence_list.shape[0]):
-	        idxs = ((np.arange(0, ML, dtype=np.int64) + np.zeros((sequence_list.shape[0],1), dtype=np.int64)) + sequence_pos[:, None])
-	        idxs = idxs + np.repeat(np.arange(0,sequence_list.shape[0]*sequence_list.shape[1], sequence_list.shape[1]), ML, axis=0).reshape(-1,ML)
-	        sequences = np.take(sequence_list, idxs)
-	        sequences = np.vstack((sequences[:i] ,sequences[i+1:])).T
-	        motif[:,0] = np.count_nonzero(sequences=="A", axis=1)
-	        motif[:,1] = np.count_nonzero(sequences=="C", axis=1)
-	        motif[:,2] = np.count_nonzero(sequences=="G", axis=1)
-	        motif[:,3] = np.count_nonzero(sequences=="T", axis=1)
-	        motif = motif/sequences.shape[1]
-	        sequence_pos[i] = find_best_pos(sequence_list[i], motif, background[i], ML)
-	        #print(motif)
-	idxs = ((np.arange(0, ML, dtype=np.int64) + np.zeros((sequence_list.shape[0],1), dtype=np.int64)) + sequence_pos[:, None])
+    start = timeit.default_timer()
+    sequence_list = np.array([[i for i in x] for x in sequence_strs])
+    background = np.zeros((sequence_list.shape[0], 4))
+    background[:,0] = np.count_nonzero(sequence_list=="A", axis=1)
+    background[:,1] = np.count_nonzero(sequence_list=="C", axis=1)
+    background[:,2] = np.count_nonzero(sequence_list=="G", axis=1)
+    background[:,3] = np.count_nonzero(sequence_list=="T", axis=1)
+    background = background/sequence_list.shape[1]
+    sequence_pos = np.random.randint(0, sequence_list.shape[1]-ML+1, (sequence_list.shape[0]))
+    prev_sequence_pos = np.full_like(sequence_pos, -1)
+    motif = np.zeros((ML, 4))
+    while not np.array_equal(prev_sequence_pos, sequence_pos):
+        prev_sequence_pos = sequence_pos
+        for i in range(sequence_list.shape[0]):
+            idxs = ((np.arange(0, ML, dtype=np.int64) + np.zeros((sequence_list.shape[0],1), dtype=np.int64)) + sequence_pos[:, None])
+            idxs = idxs + np.repeat(np.arange(0,sequence_list.shape[0]*sequence_list.shape[1], sequence_list.shape[1]), ML, axis=0).reshape(-1,ML)
+            sequences = sequence_list.flatten()[idxs].reshape(-1, ML)
+            sequences = np.vstack((sequences[:i] ,sequences[i+1:])).T
+            motif[:,0] = np.count_nonzero(sequences=="A", axis=1)
+            motif[:,1] = np.count_nonzero(sequences=="C", axis=1)
+            motif[:,2] = np.count_nonzero(sequences=="G", axis=1)
+            motif[:,3] = np.count_nonzero(sequences=="T", axis=1)
+            motif = motif/sequences.shape[1]
+            sequence_pos[i] = find_best_pos(sequence_list[i], motif, background[i], ML)
+            #print(motif)
+    idxs = ((np.arange(0, ML, dtype=np.int64) + np.zeros((sequence_list.shape[0],1), dtype=np.int64)) + sequence_pos[:, None])
     idxs = idxs + np.repeat(np.arange(0,sequence_list.shape[0]*sequence_list.shape[1], sequence_list.shape[1]), ML, axis=0).reshape(-1,ML)
-    sequences = np.take(sequence_list, idxs).T
-	motif[:,0] = np.count_nonzero(sequences=="A", axis=1)
+    sequences = sequence_list.flatten()[idxs].reshape(-1, ML).T
+    motif[:,0] = np.count_nonzero(sequences=="A", axis=1)
     motif[:,1] = np.count_nonzero(sequences=="C", axis=1)
     motif[:,2] = np.count_nonzero(sequences=="G", axis=1)
     motif[:,3] = np.count_nonzero(sequences=="T", axis=1)
-	return motif/sequences.shape[1]
+    stop = timeit.default_timer()
+    return motif/sequences.shape[1], sequence_pos, stop-start
+
+
+
+for x in os.scandir('dataset'):
+    for i in range(1, 11):
+        f = open(os.path.join(x.path, str(i), 'sequences.fasta'), 'r')
+        g = open(os.path.join(x.path, str(i), 'motiflength.txt'), 'r')
+        ML = int(g.readline())
+        fasta_sequences = SeqIO.parse(f,'fasta')
+        sequence_strs = []
+        for fasta in fasta_sequences:
+            sequence_strs.append(str(fasta.seq))
+        motif, sites, time = gibbs_find_motif(sequence_strs, ML)
+        print(x.path)
+        print(motif)
+        f.close()
+        g.close()
+'''f = open("dataset/default/1/sequences.fasta", 'r')
+fasta_sequences = SeqIO.parse(f,'fasta')
+sequence_strs = []
+for fasta in fasta_sequences:
+    sequence_strs.append(str(fasta.seq))
+motif, sites, _ = gibbs_find_motif(sequence_strs, 8)
+print(motif)
+print(sites)
+count = 0
+while sites[0] != 341:
+    motif, sites, _ = gibbs_find_motif(sequence_strs, 8)
+    print(sites)
+    count +=1
+
+print(count)
+print(motif)'''
