99497d052de0cca379c36b0a2223a3dc85360d4d
unknown <michael.cheng65@gmail.com>
2022-04-26 17:09:20 -0500

    Fix motif finder

--- motif_finder.py
+++ motif_finder.py
@@ -11,18 +11,23 @@ def find_best_pos(sequence, motif, background, ML):
     best_pos = 0
     best_score = -np.inf
     nuc_dict = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+    scores = np.zeros(len(sequence)-ML+1)
     for i in range(len(sequence)-ML+1):
         score = 0
         for j in range(i, i+ML):
             nuc = nuc_dict[sequence[j]]
             if motif[j-i, nuc] == 0 or background[nuc] == 0:
-                score = -np.inf
+                score = 0
                 break
             score += math.log(motif[j-i, nuc]/background[nuc])
-        if score > best_score:
-            best_score = score
-            best_pos = i
-    return best_pos
+        scores[i] = score
+    #print(scores)
+    #print(np.exp(scores))
+    if np.sum(np.exp(scores)) == 0:
+        return np.random.choice(len(sequence)-ML+1)
+    scores = np.exp(scores)/np.sum(np.exp(scores))
+    #print(scores)
+    return np.random.choice(len(sequence)-ML+1, p=scores)
 
 def gibbs_find_motif(sequence_strs, ML):
     start = timeit.default_timer()
@@ -36,8 +41,11 @@ def gibbs_find_motif(sequence_strs, ML):
     sequence_pos = np.random.randint(0, sequence_list.shape[1]-ML+1, (sequence_list.shape[0]))
     prev_sequence_pos = np.full_like(sequence_pos, -1)
     motif = np.zeros((ML, 4))
-    while not np.array_equal(prev_sequence_pos, sequence_pos):
-        prev_sequence_pos = sequence_pos
+    count = 0 
+    while not np.array_equal(prev_sequence_pos, sequence_pos) and count < 800:
+        count += 1
+        prev_sequence_pos = np.copy(sequence_pos)
+        #print(sequence_pos)
         for i in range(sequence_list.shape[0]):
             idxs = ((np.arange(0, ML, dtype=np.int64) + np.zeros((sequence_list.shape[0],1), dtype=np.int64)) + sequence_pos[:, None])
             idxs = idxs + np.repeat(np.arange(0,sequence_list.shape[0]*sequence_list.shape[1], sequence_list.shape[1]), ML, axis=0).reshape(-1,ML)
@@ -58,8 +66,8 @@ def gibbs_find_motif(sequence_strs, ML):
     motif[:,2] = np.count_nonzero(sequences=="G", axis=1)
     motif[:,3] = np.count_nonzero(sequences=="T", axis=1)
     stop = timeit.default_timer()
-    return motif/sequences.shape[1], sequence_pos, (stop-start)*1000
-
+    print(count)
+    return motif/sequences.shape[1], sequence_pos, (stop-start)
 
 if __name__ == "__main__":
 
@@ -81,13 +89,14 @@ if __name__ == "__main__":
             site_strs[-1] = str(sites[-1])
             k.writelines(site_strs)
             j.write(str(time))
-            print(x.path)
+            print(x.path, i)
             print(motif)
             f.close()
             g.close()
             k.close()
             j.close()
-    '''f = open("dataset/default/1/sequences.fasta", 'r')
+    '''
+    f = open("dataset/default/1/sequences.fasta", 'r')
     fasta_sequences = SeqIO.parse(f,'fasta')
     sequence_strs = []
     for fasta in fasta_sequences:
@@ -96,9 +105,10 @@ if __name__ == "__main__":
     print(motif)
     print(sites)
     count = 0
-    while sites[0] != 341:
+    while sites[0] != 165:
         motif, sites, _ = gibbs_find_motif(sequence_strs, 8)
         print(sites)
+        print(motif)
         count +=1
 
     print(count)
