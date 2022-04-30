dbd3c84ad08fb0edf48ab3624c7e4e5adc7604a5
RomeroRIsa <rrisa2@illinois.edu>
2022-04-30 15:37:27 -0500

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -2,6 +2,7 @@ from filecmp import dircmp
 import os
 import numpy as np
 import numpy.linalg as la
+import statistics
 
 
 def relative_entropy(motif, predicted_motif):
@@ -16,6 +17,7 @@ def relative_entropy(motif, predicted_motif):
     myMotif = []
     myPredictedMotif = []
     entropy = 0 
+    
     
     with open(motif, 'r') as motif_file:
         motif_lines = motif_file.readlines()
@@ -70,32 +72,62 @@ def sites_norm(sites, predicted_sites):
     
     return norm, count
 
+def gibbs_runtime(runtime):
+    with open(runtime, 'r') as runtime_file:
+        myRuntime = runtime_file.readlines()
+    
+    return float(myRuntime[0])
+
 
 directories = ['default', 'ICPC_1', 'ICPC_1.5', 'ML_6', 'ML_7', 'SC_5', 'SC_20'] 
 dirname = os.path.dirname(__file__)
 entropies = []
 norm_list = []
 count_list = []
+runtime_list = []
+entropy_error = []
+norm_error = []
+runtime_error = []
+count_error = []
 
 for directory in directories:
     dir_entropy = []
     dir_norm = []
     dir_count = []
+    dir_runtime = []
+    dir_entropy_error = []
+    dir_norm_error = []
+    dir_runtime_error = []
     for i in range(1, 11):
         predicted_motif_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/predictedmotif.txt')
         motif_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/motif.txt')
         sites_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/sites.txt')
         predicted_sites_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/predictedsites.txt')
+        runtime_file = os.path.join(dirname, 'dataset/'+ directory + '/' + str(i) + '/runtime.txt')
         
-        dir_entropy.append(relative_entropy(motif_file, predicted_motif_file))
+        entropy = relative_entropy(motif_file, predicted_motif_file)
+        dir_entropy.append(entropy)
         norm, count = sites_norm(sites_file, predicted_sites_file)
         dir_norm.append(norm)
         dir_count.append(count)
+        runtime = gibbs_runtime(runtime_file)
+        dir_runtime.append(runtime)
+        
     entropies.append(sum(dir_entropy)/len(dir_entropy))
     norm_list.append(sum(dir_norm)/len(dir_norm))
     count_list.append(sum(dir_count)/len(dir_count))
+    runtime_list.append(sum(dir_runtime)/len(dir_runtime))
+    entropy_error.append(statistics.pstdev(dir_entropy)/np.sqrt(len(dir_entropy)))
+    norm_error.append(statistics.pstdev(dir_norm)/np.sqrt(len(dir_norm)))
+    runtime_error.append(statistics.pstdev(dir_runtime)/np.sqrt(len(dir_runtime)))
+    count_error.append(statistics.pstdev(dir_count)/np.sqrt(len(dir_count)))
+    
         
-print(entropies)
+#print(entropies)
 print(norm_list)
-print(count_list)
-       
+print("overlapping:", count_list)
+print("runtime", runtime_list)
+print(entropy_error)
+print(norm_error)
+print("runtime", runtime_error)       
+print("overlap:", count_error)
