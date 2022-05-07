4ec701c3a89d17b036a6c9cff6da6958f9aafdb4
RomeroRIsa <rrisa2@illinois.edu>
2022-05-07 15:02:13 -0500

    Update motif_finder.py

--- motif_finder.py
+++ motif_finder.py
@@ -8,8 +8,6 @@ from generate import convert_motif
 
 
 def find_best_pos(sequence, motif, background, ML):
-    best_pos = 0
-    best_score = -np.inf
     nuc_dict = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
     scores = np.zeros(len(sequence)-ML+1)
     for i in range(len(sequence)-ML+1):
@@ -21,12 +19,9 @@ def find_best_pos(sequence, motif, background, ML):
                 break
             score += math.log(motif[j-i, nuc]/background[nuc])
         scores[i] = score
-    #print(scores)
-    #print(np.exp(scores))
     if np.sum(np.exp(scores)) == 0:
         return np.random.choice(len(sequence)-ML+1)
     scores = np.exp(scores)/np.sum(np.exp(scores))
-    #print(scores)
     return np.random.choice(len(sequence)-ML+1, p=scores)
 
 def gibbs_find_motif(sequence_strs, ML):
@@ -45,7 +40,6 @@ def gibbs_find_motif(sequence_strs, ML):
     while not np.array_equal(prev_sequence_pos, sequence_pos) and count < 2000:
         count += 1
         prev_sequence_pos = np.copy(sequence_pos)
-        #print(sequence_pos)
         for i in range(sequence_list.shape[0]):
             idxs = ((np.arange(0, ML, dtype=np.int64) + np.zeros((sequence_list.shape[0],1), dtype=np.int64)) + sequence_pos[:, None])
             idxs = idxs + np.repeat(np.arange(0,sequence_list.shape[0]*sequence_list.shape[1], sequence_list.shape[1]), ML, axis=0).reshape(-1,ML)
@@ -57,7 +51,6 @@ def gibbs_find_motif(sequence_strs, ML):
             motif[:,3] = np.count_nonzero(sequences=="T", axis=1)
             motif = motif/sequences.shape[1]
             sequence_pos[i] = find_best_pos(sequence_list[i], motif, background[i], ML)
-            #print(motif)
     idxs = ((np.arange(0, ML, dtype=np.int64) + np.zeros((sequence_list.shape[0],1), dtype=np.int64)) + sequence_pos[:, None])
     idxs = idxs + np.repeat(np.arange(0,sequence_list.shape[0]*sequence_list.shape[1], sequence_list.shape[1]), ML, axis=0).reshape(-1,ML)
     sequences = sequence_list.flatten()[idxs].reshape(-1, ML).T
